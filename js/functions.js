2ff976b7cbfe4426040db51a9f86682b59f34bd4
Cory Musick <cory.musick37@gmail.com>
2019-02-08 09:42:54 -0600

    working on bonuses

--- js/functions.js
+++ js/functions.js
@@ -1,117 +1,232 @@
 "use strict";
 
-/**
- * TODO:
- * Create a function called 'sayHello' that takes a parameter 'name'.
- * When called, the function should return a message that says hello to the passed in name.
- *
- * Example
- * > sayHello("codeup") // returns "Hello, codeup!"
- */
-(function()
-{
-    function sayHello(name) {
-        return 'hello, ' + name + '!';
+// /**
+//  * TODO:
+//  * Create a function called 'sayHello' that takes a parameter 'name'.
+//  * When called, the function should return a message that says hello to the passed in name.
+//  *
+//  * Example
+//  * > sayHello("codeup") // returns "Hello, codeup!"
+//  */
+// // (function()
+// // {
+//     function sayHello(name) {
+//         return 'hello, ' + name + '!';
+//     }
+//
+//     /**
+//      * TODO:
+//      * Call the function 'sayHello' and pass your name as a string literal argument.
+//      * Store the result of the function call in a variable named 'helloMessage'.
+//      *
+//      * console.log 'helloMessage' to check your work
+//      */
+//     var helloMessage = sayHello('Cory');
+//
+//     console.log(helloMessage);
+//
+//     /**
+//      * TODO:
+//      * Store your name as a string in a variable named 'myName', and pass that
+//      * variable to the 'sayHello' function. You should see the same output in the
+//      * console.
+//      */
+//
+//     var myName = 'Cory';
+//
+//     console.log(sayHello(myName));
+//
+//
+// // Don't modify the following line, it generates a random number between 1 and 3
+// // and stores it in a variable named random
+//     var random = Math.floor((Math.random() * 3) + 1);
+//
+//     /**
+//      * TODO:
+//      * Create a function called 'isTwo' that takes a number as a parameter.
+//      * The function should return a boolean value based on whether or not the passed
+//      * number is the number 2.
+//      *
+//      * Example
+//      * > isTwo(1) // returns false
+//      * > isTwo(2) // returns true
+//      * > isTwo(3) // returns false
+//      *
+//      * Call the function 'isTwo' passing the variable 'random' as a argument.
+//      *
+//      * console.log *outside of the function* to check your work (you should see a
+//      * different result everytime you refresh the page if you are using the random
+//      * number)
+//      */
+//     function isTwo(num) {
+//         return num === 2;
+//     }
+//
+//     console.log(isTwo(random));
+//
+//     /**
+//      * TODO:
+//      * Create a function named 'calculateTip' to calculate a tip on a bill at a
+//      * restaurant. The function should accept a tip percentage and the total of the
+//      * bill, and return the amount to tip
+//      *
+//      * Examples:
+//      * > calculateTip(0.20, 20) // returns 4
+//      * > calculateTip(0.25, 25.50) // returns 6.37
+//      * > calculateTip(0.15, 33.42) // returns 5.013
+//      */
+//     function calculateTip(tipPercent, billTotal) {
+//         return tipPercent * billTotal;
+//     }
+//
+//     /**
+//      * TODO:
+//      * Use prompt and alert in combination with your calculateTip function to
+//      * prompt the user for the bill total and a percentage they would like to tip,
+//      * then display the dollar amount they should tip
+//      */
+//     var billTotal = prompt("What is the bill total without the dollar sign.");
+//     var tipPercent = prompt('Enter the percent you would like to tip as a decimal');
+//
+//     alert('You should tip ' + calculateTip(tipPercent, billTotal) + ' dollars. ');
+//     /**
+//      * TODO:
+//      * Create a function named `applyDiscount`. This function should accept a price
+//      * (before a discount is applied), and a discount percentage (a number between 0
+//      * and 1). It should return the result of applying the discount to the original
+//      * price.
+//      *
+//      * Example:
+//      * > var originalPrice = 100;
+//      * > var dicountPercent = .2; // 20%
+//      * > applyDiscount(originalPrice, discountPercent) // 80
+//      *
+//      * > applyDiscount(45.99, 0.12) // 40.4712
+//      */
+//
+//     var originalPrice = 45.99;
+//     var discountPercent = .12;
+//
+//     function applyDiscount(price, discountPercent) {
+//         return price - (price * discountPercent);
+//     }
+//
+//     console.log(applyDiscount(originalPrice, discountPercent));
+
+//isOdd
+    function isOdd(number){
+        return number % 2 === 1;
     }
 
-    /**
-     * TODO:
-     * Call the function 'sayHello' and pass your name as a string literal argument.
-     * Store the result of the function call in a variable named 'helloMessage'.
-     *
-     * console.log 'helloMessage' to check your work
-     */
-    var helloMessage = sayHello('Cory');
+    console.log(isOdd(13)); // true
+    console.log(isOdd(144)); // false
 
-    console.log(helloMessage);
-
-    /**
-     * TODO:
-     * Store your name as a string in a variable named 'myName', and pass that
-     * variable to the 'sayHello' function. You should see the same output in the
-     * console.
-     */
-
-    var myName = 'Cory';
-
-    console.log(sayHello(myName));
-
-
-// Don't modify the following line, it generates a random number between 1 and 3
-// and stores it in a variable named random
-    var random = Math.floor((Math.random() * 3) + 1);
-
-    /**
-     * TODO:
-     * Create a function called 'isTwo' that takes a number as a parameter.
-     * The function should return a boolean value based on whether or not the passed
-     * number is the number 2.
-     *
-     * Example
-     * > isTwo(1) // returns false
-     * > isTwo(2) // returns true
-     * > isTwo(3) // returns false
-     *
-     * Call the function 'isTwo' passing the variable 'random' as a argument.
-     *
-     * console.log *outside of the function* to check your work (you should see a
-     * different result everytime you refresh the page if you are using the random
-     * number)
-     */
-    function isTwo(num) {
-        return num === 2;
+//isEven
+    function isEven(number){
+        return number % 2 === 0;
     }
 
-    console.log(isTwo(random));
+    console.log(isEven(13)); // false
+    console.log(isEven(144)); // true
 
-    /**
-     * TODO:
-     * Create a function named 'calculateTip' to calculate a tip on a bill at a
-     * restaurant. The function should accept a tip percentage and the total of the
-     * bill, and return the amount to tip
-     *
-     * Examples:
-     * > calculateTip(0.20, 20) // returns 4
-     * > calculateTip(0.25, 25.50) // returns 6.37
-     * > calculateTip(0.15, 33.42) // returns 5.013
-     */
-    function calculateTip(tipPercent, billTotal) {
-        return tipPercent * billTotal;
+//identity
+    function identity(input){
+        return input;
     }
 
-    /**
-     * TODO:
-     * Use prompt and alert in combination with your calculateTip function to
-     * prompt the user for the bill total and a percentage they would like to tip,
-     * then display the dollar amount they should tip
-     */
-    var billTotal = prompt("What is the bill total without the dollar sign.");
-    var tipPercent = prompt('Enter the percent you would like to tip as a decimal');
+    console.log(identity('hello')); // hello
+    console.log(identity(7)); // 7
 
-    alert('You should tip ' + calculateTip(tipPercent, billTotal) + ' dollars. ');
-    /**
-     * TODO:
-     * Create a function named `applyDiscount`. This function should accept a price
-     * (before a discount is applied), and a discount percentage (a number between 0
-     * and 1). It should return the result of applying the discount to the original
-     * price.
-     *
-     * Example:
-     * > var originalPrice = 100;
-     * > var dicountPercent = .2; // 20%
-     * > applyDiscount(originalPrice, discountPercent) // 80
-     *
-     * > applyDiscount(45.99, 0.12) // 40.4712
-     */
-
-    var originalPrice = 45.99;
-    var discountPercent = .12;
-
-    function applyDiscount(price, discountPercent) {
-        return price - (price * discountPercent);
+//isFive
+    function isFive(){
+        return 5;
     }
 
-    console.log(applyDiscount(originalPrice, discountPercent));
+    console.log(isFive()); // 5
+
+// addFive
+    function addFive(input){
+        return input + isFive()
+    }
+
+    console.log(addFive(15)); // 20
+
+//isMultipleOfFive
+    function isMultipleOfFive(input){
+        return input % isFive() === 0
+    }
+
+console.log(isMultipleOfFive(14)); // false
+console.log(isMultipleOfFive(25)); //true
+
+//isThree
+    function isThree(){
+        return 3
+    }
+
+console.log(isThree()); // 3
+
+//isMultipleOfThree
+    function isMultipleOfThree(input){
+        return input % isThree() === 0;
+    }
+
+console.log(isMultipleOfThree(9)); // true
+console.log(isMultipleOfThree(16)); //false
+
+//isMultipleOfThreeAndFive
+    function isMultipleOfThreeAndFive(input){
+        return (input % isThree() === 0) && (input % isFive() === 0)
+    }
+
+console.log(isMultipleOfThreeAndFive(15)); // true
+console.log(isMultipleOfThreeAndFive(9)); // false
+console.log(isMultipleOfThreeAndFive(5)); // false
+
+// isMultipleOf
+    function isMultipleOf(target, n){
+        return target % n === 0;
+    }
+
+console.log(isMultipleOf(15, 5)); // true
+console.log(isMultipleOf(12, 10)); // false
+
+//isTrue
+    function isTrue(boolean){
+        return boolean === true;
+    }
+
+console.log(isTrue(true)); // true
+console.log(isTrue(false)); // false
+
+//isFalse
+    function isFalse(boolean){
+        return boolean === false;
+}
+
+console.log(isFalse(true)); // false
+console.log(isFalse(false)); // true
+
+//isTruthy
+    function isTruthy(input){
+        return input == true;
+    }
+
+console.log(isTruthy(false)); // false
+console.log(isTruthy(1)); // true
+console.log(isTruthy(15)) // false
+console.log(isTruthy(true)) // true
+
+//isFalsy
+    function isFalsy(input){
+        return input == false;
+
+    }
+
+console.log(isFalsy(false)); // true
+console.log(isFalsy(0)); // true
+console.log(isFalsy(15)) // false
+console.log(isFalsy(true)) // false
 
 
-}());
+// }());
