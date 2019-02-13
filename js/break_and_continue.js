49427deac795e43853e21ba7f92b431328dc8df9
Cory Musick <cory.musick37@gmail.com>
2019-02-13 16:50:32 -0600

    working on books portion of exercise need to make loop

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,8 +1,20 @@
 for(var i = 1; i > 0; ){
     var input = prompt("Please enter an odd number between 1 and 50");
-    if( input <= 50 && input >= 1 && input % 2 === 1){
+    if( isNaN(input)){
+        alert('That is not a number, please try again.');
+        continue
+    }
+    if (input > 50 || input < 1){
+        alert('That number is not in range, please try again');
+        continue
+    }
+    if( input % 2 === 0){
+        alert('ODD number please.');
+        continue
+    }else{
         break;
     }
+
 }
 
 for(var j = 1; j <= 50; j++){
--- js/while.js
+++ js/while.js
@@ -1,24 +1,26 @@
-var i = 0;
-var x = 1;
-
-while ( i <= 15){
-
-    console.log(x = x * 2);
-    i++;
-}
+// var i = 0;
+// var x = 1;
+//
+// while ( i <= 15){
+//
+//     console.log(x = x * 2);
+//     i++;
+// }
 
 var allCones = Math.floor(Math.random() * 50) + 50;
 
 while (allCones >= 0){
+
     var purchasedCones = Math.floor(Math.random() * 5) + 1;
+
     if (allCones >= purchasedCones && allCones !== 0){
-    allCones = allCones - purchasedCones;
-    alert(purchasedCones + " cones sold..." + allCones + " cones left")
+    allCones -= purchasedCones;
+    console.log(purchasedCones + " cones sold..." + allCones + " cones left")
     }else if(purchasedCones > allCones && allCones !== 0){
-        alert("Cannot sell you " + purchasedCones + " cones I only have " + allCones)
+        console.log("Cannot sell you " + purchasedCones + " cones I only have " + allCones)
 
     }else{
-        alert('Yay I sold them all!');
+        console.log('Yay I sold them all!');
     break;
     }
 }
