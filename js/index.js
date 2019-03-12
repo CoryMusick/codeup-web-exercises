05dd75e77b34c491cdd38f74ab37c44ec6bad600
Cory Musick <cory.musick37@gmail.com>
2019-03-12 09:31:26 -0500

    exercise done w/ both import and require index.html made to test

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ const data = require('./say-hello.js');
 
 data.sayHello();
 data.sayGoodbye();
+
+import message from "./say-hello.js"
+
+console.log(message.niceMessage());
--- js/say-hello.js
+++ js/say-hello.js
@@ -1,9 +1,13 @@
-function sayHello(){
-    console.log('Hello!')
-}
-function sayGoodbye(){
-    console.log('Goodbye!')
-}
+const sayHello = () => console.log('Hello!')
+
+const sayGoodbye = () => console.log('Goodbye!')
 
 
 module.exports = {sayHello, sayGoodbye}
+
+const niceMessage = () => {
+    return "A Nice Message"
+}
+
+export default{niceMessage}
+
